7aa57dd04afc57d1afe3543a7151618a20a0d8e8
yeenbean <gitkraken@be903fb8-5e96-454d-9af3-1c9dc2ba5f96.com>
2024-06-19 13:01:44 -0700

    🔧 Enable Flakes support

--- configuration.nix
+++ configuration.nix
@@ -163,5 +163,9 @@
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05"; # Did you read the comment?
 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 }
 
